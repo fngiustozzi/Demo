7a51549bf13ff9d4c7d3b9ee6de97917f628ffad
Franco Giustozzi <franco.giustozzi@insa-rouen.fr>
2021-03-25 21:49:17 +0100

    Initial commit

--- src/main/java/com/example/demo/CarModelAssembler.java
+++ src/main/java/com/example/demo/CarModelAssembler.java
@@ -5,7 +5,7 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
-
+//
 @Component
 class CarModelAssembler implements RepresentationModelAssembler<Car, EntityModel<Car>> {
 
